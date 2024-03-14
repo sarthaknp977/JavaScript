2b0ca0ae9095518ce018e7ad39ad15bad5ea3e64
sarthaknp977 <sarthaknp977@gmail.com>
2024-03-14 01:39:49 +0000

    Edit 1

--- newThingsIGotIntroducedTo/script.js
+++ newThingsIGotIntroducedTo/script.js
@@ -1,2 +1,28 @@
-//console.table([a,b,c,d])
-//this will print the data in tabular form
+/*Number One:
+
+console.table([a,b,c,d])
+this will print the data in tabular form
+
+
+command
+console.table([1,2,3,4,5])
+
+ output :
+┌─────────┬────────┐
+│ (index) │ Values │
+├─────────┼────────┤
+│ 0       │ 1      │
+│ 1       │ 2      │
+│ 2       │ 3      │
+│ 3       │ 4      │
+│ 4       │ 5      │
+└─────────┴────────┘
+*/
+
+/*
+Number two:
+variableName=value
+
+yes this will work fine even tho there's no variable type specified
+However dont use it 
+*/
