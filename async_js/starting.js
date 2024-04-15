f8647ec3c423c462dd3a5690da4d4d15d685bb9f
sarthaknp977 <sarthaknp977@gmail.com>
2024-04-15 20:20:18 +0545

    Async almost complete

--- async_js/starting.js
+++ async_js/starting.js
@@ -111,20 +111,20 @@
 //if the promise returns with the value then it changes to resolved 
 //else if the promise doesn't return with with a desired/wrong data, the value changes to rejected
 
-const wait = new Promise(function (resolve, reject) {
-    fetch('https://catfact.ninja/fact')
-        .then(raw => raw.json())
-        .then(result => {
-            if (result.fact.includes("cat")) {
-                resolve();
-            }
-            else {
-                reject();
-            }
-        })
-});
+// const wait = new Promise(function (resolve, reject) {
+//     fetch('https://catfact.ninja/fact')
+//         .then(raw => raw.json())
+//         .then(result => {
+//             if (result.fact.includes("cat")) {
+//                 resolve();
+//             }
+//             else {
+//                 reject();
+//             }
+//         })
+// });
 
-
+/*
 wait
     .then(function () {         // runs only when promise hold resolve
         console.log("done")
@@ -133,5 +133,211 @@ wait
     .catch(function () {            // runs only when promise hold reject
         console.log("not done")
     })
+    */
 
 // console.log(wait);//if printed outside this will return pending as this is on the main stack and the wait(promise) is on side stack
+
+
+
+// callback: Nothing but a funcion, The unique / special part is that, it is passed as an argument to some async function,when some sync code run then only you can use that that callback function
+
+// How to use callback function?
+// -its basically a function so like every other function we write something that we want to execute, and we run this code when our async code is already executed
+
+
+// function Callbacks(url, callback) {
+//     fetch(url)
+//         .then(raw => raw.json())
+//         .then(result => {
+//             callback(result);
+//         })
+// }
+
+// Callbacks('https://official-joke-api.appspot.com/random_joke', function (result) {
+//     console.log(result.setup)
+//     console.log(result.punchline)
+// });
+
+
+// Async / Await : Function is necessary everytime while using async/await
+
+// make any function and write any code that you want, when using async the lines after the async line run before as the async code goes to side stack, if the line after async is dependend upon async then we will get an error as there is no any value receivede from async code
+//eg :
+// a = fetch('https://official-joke-api.appspot.com/random_joke');
+// console.log(a); //This will give pending as there no value returned to a as fetching isnt completed while this line is executed
+
+// however using await we can fix this problem, as await will ask the line to wait until the code is completed
+// but to use await, we have to use async in the parent function ie. await is only valid in async functions and the top level bodies of modules
+
+
+
+// with async await, we can write async code as if it was a regular synchronous code
+// (async () => {
+//     const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+//     const joke = await response.json()
+//     console.log(joke.setup);
+// })();
+
+
+
+// callbacks vs promises vs async/await
+
+
+// Using Callback
+// function usingcallback(url, callback) {
+//     fetch(url)
+//         .then(raw => raw.json())
+//         .then(result => {
+//             callback(result);
+//         })
+// }
+// testcallback('https://official-joke-api.appspot.com/random_joke', function (result) {
+//     console.log(result.setup);
+// })
+
+// Using Promises
+// const promises = new Promise(function (resolve, reject) {
+//     fetch('https://official-joke-api.appspot.com/random_joke')
+//         .then(raw => raw.json())
+//         .then(result => resolve(result))
+// })
+
+// promises
+//     .then(function (result) {
+//         console.log(result.setup);
+//     })
+
+//     .catch(function () {
+//         console.log("Error!!!");
+//     })
+
+
+// Using await/async
+
+// async function usingawait() {
+//     const joke = await fetch('https://official-joke-api.appspot.com/random_joke')
+//     const mainJoke = await joke.json();
+//     console.log(mainJoke.setup);
+// }
+
+// usingawait();
+
+//                                OR
+
+// async function usingawait() {
+//     const joke = await fetch('https://official-joke-api.appspot.com/random_joke')
+//     const mainJoke = await joke.json();
+//     return mainJoke;
+// }
+
+// async function sayJoke() {
+//     const mainJoke = await usingawait();
+//     console.log(mainJoke.setup);
+// }
+
+// sayJoke();
+
+
+// Event Loop
+
+// As there are 2 working area to run js code ie main stack and side stack
+
+// the tasks from side stack have to wait until the main stack is empty, this checking is done by something called event loop
+// This event loop continiously checks either the main stack is empty or not and then sends the tasks from side stack to main stack one at a time 
+
+
+//Generators
+
+// They are used to pause at a certain line until we say next or to pause the flow of execution
+
+// Normal function
+// function Loop() {
+//     for (let a = 0; a < 10; a++) {
+//         console.log(a + 1);
+//     }
+// }
+
+
+// function with Generators(uses * like pointers)
+
+// function* Loop() {
+//     let x = 0;
+//     for (let a = 1; a < 11; a++) {
+//         if (a % 2 == 0) {
+//             yield x;
+//             x++
+//         }
+//     }
+// }
+
+
+// function* Loop2() {
+
+//     for (let a = 1; a < 11; a++)    yield a;
+// }
+// const ans = Loop();
+// const ans2 = Loop2();
+/*
+console.log(ans.next().value);
+console.log(ans.next().value);
+console.log(ans.next().value);
+console.log(ans.next().value);
+console.log(ans.next().value);
+console.log(ans.next().value); //undefined
+console.log(ans.next().value); //undefined
+console.log(ans.next().value); //undefined
+console.log(ans.next().value); //undefined
+console.log(ans.next().value); //undefined
+*/
+
+
+//all defined
+
+
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+// console.log(ans2.next().value);
+
+//                              OR
+
+// for (let i = 0; i < 12; i++) {
+//     console.log(ans2.next().value);
+// }
+
+
+// for prime Numbers using generators
+
+// function isPrime(i) {
+//     for (let index = 2; index < i; index++) {
+//         if (i % index == 0) return false;
+//     }
+//     return true;
+// }
+// function* generatorsPrime() {
+//     for (let i = 1; i < 100; i++) {
+//         if (isPrime(i)) {
+//             yield i;
+//         }
+//     }
+// }
+
+// const totalcount = generatorsPrime();
+// for(let i=1;i<27;i++){
+//     console.log(totalcount.next().value);
+// }
+
+
+
+
+
+
+
+
+
+//Web Workers
